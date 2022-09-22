c41fc1ff26b53b971c5d86c74f79faa0aa0deb81
defr0stt <dima.sb713@gmail.com>
2022-09-22 14:37:31 +0300

    Initial commit

---
+++ src/main/java/ua/lpnu/clearsolutionstask/ClearSolutionsTaskApplication.java
@@ -0,0 +1,13 @@
+package ua.lpnu.clearsolutionstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearSolutionsTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearSolutionsTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/lpnu/clearsolutionstask/ClearSolutionsTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lpnu.clearsolutionstask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionsTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
